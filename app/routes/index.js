4f1ca1bdd6cecc823a702ab9b389a2ecf97e57b5
hdwatts <howard@hdwatts.com>
2016-06-20 15:40:44 -0400

    Added full refresh redirect

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   actions: {
     enter(params){
       if ( params != "" ) {
-        this.transitionTo('game',{queryParams: {username: params}})
+        window.location = "/game?username="+params
       }
     }
   }
