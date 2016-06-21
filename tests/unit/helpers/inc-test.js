7b76009d9fc50fba43a54610aadab0bbb4a9d47d
hdwatts <howard@hdwatts.com>
2016-06-21 11:14:01 -0400

    Added incremented index to scores

---
+++ tests/unit/helpers/inc-test.js
@@ -0,0 +1,10 @@
+import { inc } from 'potato-frontend/helpers/inc';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | inc');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = inc([42]);
+  assert.ok(result);
+});
