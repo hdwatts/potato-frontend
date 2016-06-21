d593bb76db6fc2376c2d8f7a128bba628520ddaf
hdwatts <howard@hdwatts.com>
2016-06-21 12:53:21 -0400

    Added https support

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: 'http://potato-backend.herokuapp.com',
+  host: 'https://potato-backend.herokuapp.com',
   namespace: 'api/v1'
 });
--- app/helpers/inc.js
+++ app/helpers/inc.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.HTMLBars.makeBoundHelper(function(indexNum){  
-  debugger;
   return indexNum[0] + 1;
 });
